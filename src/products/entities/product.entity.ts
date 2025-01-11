da67e792c63c90a492ae7118afad94b58388bbdb
Manuel_E <manuelespina7@hotmail.com>
2025-01-11 16:18:07 -0400

    Cracion de Product app nets

---
+++ src/products/entities/product.entity.ts
@@ -0,0 +1,10 @@
+import { NodeCompatibleEventEmitter } from "rxjs/internal/observable/fromEvent";
+
+export class Product {
+
+    public id: Number; 
+    public name: string;
+    public price:number;
+
+
+}
